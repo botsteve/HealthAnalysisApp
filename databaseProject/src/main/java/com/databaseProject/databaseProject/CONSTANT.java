420ed2daa5a4a7e3658816f8310d0e4fedb5e2b7
Rusen Stefan <stefan.rusen@ibm.com>
2019-07-22 20:09:03 +0300

    update

--- databaseProject/src/main/java/com/databaseProject/databaseProject/CONSTANT.java
+++ databaseProject/src/main/java/com/databaseProject/databaseProject/CONSTANT.java
@@ -3,5 +3,5 @@ package com.databaseProject.databaseProject;
 public class CONSTANT {
     private CONSTANT() {}
 //    public static final String API_ENDPOINT = "http://192.168.43.116:4200";
-    public static final String API_ENDPOINT = "http://5.12.156.255:4200";
+    public static final String API_ENDPOINT = "http://5.12.10.57:4200";
 }
--- databaseProjectFE/src/app/common/constants.ts
+++ databaseProjectFE/src/app/common/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
   // public static API_ENDPOINT = "http://192.168.43.116:8080/";
-  public static API_ENDPOINT = "http://5.12.156.255:8080/";
+  public static API_ENDPOINT = "http://5.12.10.57:8080/";
 }
