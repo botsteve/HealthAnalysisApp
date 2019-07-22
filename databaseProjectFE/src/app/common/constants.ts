43353c6d0a0994592cc6b89e11abb9041952e45b
Rusen Stefan <stefan.rusen@ibm.com>
2019-07-22 23:51:16 +0300

    up

--- databaseProjectFE/src/app/common/constants.ts
+++ databaseProjectFE/src/app/common/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
   // public static API_ENDPOINT = "http://192.168.43.116:8080/";
-  public static API_ENDPOINT = "http://5.12.161.96:8080/";
+  public static API_ENDPOINT = "http://192.168.43.103:8080/";
 }
