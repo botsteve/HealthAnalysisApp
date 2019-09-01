8a2e13a4fd46e15e27da925c51eb368d3cb73693
Rusen Stefan <stefan.rusen@ibm.com>
2019-09-01 13:48:31 -0900

    update script

--- script.py
+++ script.py
@@ -10,13 +10,14 @@ import Adafruit_MCP9808.MCP9808 as MCP9808
 
 port="/dev/ttyS0"
 serArd= serial.Serial('/dev/ttyACM0', 9600)
-ser = serial.Serial(port, baudrate = 9600, timeout=0.5)
+#ser = serial.Serial(port, baudrate = 9600, timeout=0.5)
 sensor = MCP9808.MCP9808()
 sensor.begin()
 urlGps = 'http://192.168.43.103:8080/gps'
 urlTemperature = 'http://192.168.43.103:8080/temperature'
 urlEMG = 'http://192.168.43.103:8080/emg'
 urlEKG = 'http://192.168.43.103:8080/ekg'
+headers = {'content-type':'application/json'}
 
 
 def c_to_f(c):
@@ -33,39 +34,34 @@ class GPS(object):
 
 while True:
 	dataout = pynmea2.NMEAStreamReader()
-	newdata = ser.readline()
 	temp = sensor.readTempC()
 	
-	if(ser.in_waiting >0):
-        	line = ser.readline()
+	if(serArd.in_waiting >0):
+        	line = serArd.readline()
         	print(line)
-			if line[0:4]='EKG':
-				z = requests.post(urlEKG,line[4:])
-			else:
-				z = requests.post(urlEMG,line[4:])
-				
-
-
-	x = requests.post(urlTemperature, json=temp)
-	print(x.text)
-	print('Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
-	time.sleep(10.0)
-
-	print(" GET LAT AND LONG")
-	
-	if newdata[0:6] == '$GPGGA':
-		newmsg = pynmea2.parse(newdata)
-		temp = GPS(newmsg.latitude, newmsg.longitude, newmsg.altitude)
-		newTemp =  {
+		if(line[0:3]=='ECG'):
+			ekg = requests.post(urlEKG,line[4:],headers=headers)
+			print(ekg.text)
+		elif(line[0:3]=='EMG'):
+			emg = requests.post(urlEMG,line[4:],headers=headers)
+			print(emg.text)
+		elif(newdata[0:6] == '$GPGGA'):
+			newmsg = pynmea2.parse(newdata)
+			tempLocation = GPS(newmsg.latitude, newmsg.longitude, newmsg.altitude)
+			newGpsTemp =  {
 			"latitude": newmsg.latitude,
 			"longitude": newmsg.longitude,
 			"altitude": newmsg.altitude
-		}
-		print(newTemp) 
-                print(" " +str(temp.latitude) + " " + str(temp.longitude) + " " + str(temp.altitude))
-		y = requests.post(urlGps, json=newTemp)
-		lat  = newmsg.latitude
-		print(lat)
-		lng  = newmsg.longitude
-		print(lng)
-		time.sleep(10)
+			}
+			print(newGpsTemp) 
+        		print(" " +str(tempLocation.latitude) + " " + str(tempLocation.longitude) + " " + str(tempLocation.altitude))
+			y = requests.post(urlGps, json=newGpsTemp)
+			lat  = newmsg.latitude
+			print(lat)
+			lng  = newmsg.longitude
+			print(lng)
+		
+	x = requests.post(urlTemperature, json=temp)
+	print(x.text)
+	#print('Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
+	time.sleep(10.0)
