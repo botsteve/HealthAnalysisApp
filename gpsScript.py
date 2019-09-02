af7d54d2f4597b7cc09b7fddbb107139ab7479aa
Rusen Stefan <stefan.rusen@ibm.com>
2019-09-02 23:14:08 +0300

    up gpsScript.py

---
+++ gpsScript.py
@@ -0,0 +1,42 @@
+import os
+import time
+import serial
+import string
+import pynmea2
+import requests
+import json
+
+serLeo= serial.Serial('/dev/ttyACM1', 57600)
+urlGps = 'http://192.168.43.103:8080/gps'
+headers = {'content-type':'applicatio/json'}
+
+class GPS(object):
+	latitude = 0
+	longitude = 0
+	laltitude = 0
+	def __init__(self, latitude, longitude, altitude):
+		self.latitude = latitude
+   		self.longitude = longitude
+   		self.altitude = altitude
+
+while True:
+	dataout = pynmea2.NMEAStreamReader()
+	if(serLeo.in_waiting >0):
+		gpsLine = serLeo.readline()
+		print(gpsLine)
+		if(gpsLine[0:6] == '$GPGGA'):
+			#print("Am intrat in GPS CASE")
+			newmsg = pynmea2.parse(gpsLine)
+			tempLocation = GPS(newmsg.latitude, newmsg.longitude, newmsg.altitude)
+			newGpsTemp =  {
+			"latitude": newmsg.latitude,
+			"longitude": newmsg.longitude,
+			"altitude": newmsg.altitude
+			}
+			print(newGpsTemp) 
+        		print(" " +str(tempLocation.latitude) + " " + str(tempLocation.longitude) + " " + str(tempLocation.altitude))
+			y = requests.post(urlGps, json=newGpsTemp, headers=headers)
+			#lat  = newmsg.latitude
+			print(y.text)
+			#lng  = newmsg.longitude
+			#print(lng)
